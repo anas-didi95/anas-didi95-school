ca5573dfe6b582c7cfac1d3bace11e7459f672d7
Anas Juwaidi <anas.didi95@gmail.com>
2025-09-30 14:07:36 +0000

    ADD: Add handler to validate vertx user

--- app/src/main/java/com/anasdidi/school/common/config/CommonConfig.java
+++ app/src/main/java/com/anasdidi/school/common/config/CommonConfig.java
@@ -1,15 +1,24 @@
 /* (C) Anas Juwaidi Bin Mohd Jeffry. All rights reserved. */
 package com.anasdidi.school.common.config;
 
+import com.nimbusds.jwt.JWTClaimsSet;
 import io.micronaut.context.MessageSource;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.i18n.ResourceBundleMessageSource;
+import io.micronaut.security.token.jwt.validator.GenericJwtClaimsValidator;
+import io.micronaut.security.token.jwt.validator.JWTClaimsSetUtils;
 import jakarta.inject.Singleton;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Factory
+@RequiredArgsConstructor
+@Slf4j
 public class CommonConfig {
+
+  private final VertxConfig vertx;
 
   @Singleton
   MessageSource messageSource() {
@@ -19,5 +28,25 @@ public class CommonConfig {
   @Singleton
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @TraceLog
+  @Singleton
+  <T> GenericJwtClaimsValidator<T> vertxUserClaimsValidator() {
+    return (claims, request) -> {
+      log.trace("START...");
+
+      JWTClaimsSet jwt = JWTClaimsSetUtils.jwtClaimsSetFromClaims(claims);
+      String username = jwt.getSubject();
+      var user = vertx.getData(username, VertxConfig.VertxUser.class).join();
+
+      if (user.isEmpty()) {
+        log.error("Vertx user not found! {}", username);
+        return false;
+      }
+
+      log.debug("Token validated...{}", username);
+      return true;
+    };
   }
 }
