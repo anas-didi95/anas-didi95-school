ab7d9274561d8eaa1c2dfe44e61c22169ba5a265
Anas Juwaidi <anas.didi95@gmail.com>
2025-12-16 05:03:01 +0000

    MOD: Refactor code

--- web/src/utils/actions/SignInAction.ts
+++ web/src/utils/actions/SignInAction.ts
@@ -7,26 +7,29 @@ const SignInAction = (revalidate?: string[]) => {
   const client = FetchClient({ retryCount: 0 });
   const { notify } = useToast();
 
-  return {
-    action: action(async (req: ISignInReq) => {
-      try {
-        const res = await client.request("/api/v1/auth/sign-in", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ...req }),
-        });
+  const handler = async (req: ISignInReq) => {
+    try {
+      const res = await client.request("/api/v1/auth/sign-in", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...req }),
+      });
 
-        const resBody = (await res.json()) as ISignInRes;
-        return json({ ok: true, data: resBody }, { revalidate });
-      } catch (err) {
-        const error = JSON.parse((err as Error).message) as IResponseError;
-        notify(error.message, { type: "error" });
-        return json({ ok: false, data: error.message }, { revalidate });
-      }
-    }, key),
+      const resBody = (await res.json()) as ISignInRes;
+      return json({ ok: true, data: resBody }, { revalidate });
+    } catch (err) {
+      const error = JSON.parse((err as Error).message) as IResponseError;
+      notify(error.message, { type: "error" });
+      return json({ ok: false, data: error.message }, { revalidate });
+    }
+  };
+
+  return {
+    handler,
+    action: action(handler, key),
     key,
   };
 };
--- web/src/utils/queries/TokenInfoQuery.ts
+++ web/src/utils/queries/TokenInfoQuery.ts
@@ -7,25 +7,28 @@ const TokenInfoQuery = (revalidate?: string[]) => {
   const client = FetchClient({ hasAuth: true });
   const { notify } = useToast();
 
-  return {
-    query: query(async () => {
-      try {
-        const res = await client.request("/api/v1/auth/token-info", {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        });
+  const handler = async () => {
+    try {
+      const res = await client.request("/api/v1/auth/token-info", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 
-        const resBody = (await res.json()) as ITokenInfoRes;
-        return json({ ok: true, data: resBody }, { revalidate });
-      } catch (err) {
-        const error = JSON.parse((err as Error).message) as IResponseError;
-        notify(error.message, { type: "error" });
-        return json({ ok: false, data: error.message }, { revalidate });
-      }
-    }, key),
+      const resBody = (await res.json()) as ITokenInfoRes;
+      return json({ ok: true, data: resBody }, { revalidate });
+    } catch (err) {
+      const error = JSON.parse((err as Error).message) as IResponseError;
+      notify(error.message, { type: "error" });
+      return json({ ok: false, data: error.message }, { revalidate });
+    }
+  };
+
+  return {
+    handler,
+    query: query(handler, key),
     key,
   };
 };
