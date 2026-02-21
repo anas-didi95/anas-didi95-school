c50199fbe6387519453bd9805b68fcca6db7fada
Anas Juwaidi <anas.didi95@tutamail.com>
2026-02-21 02:31:19 +0000

    ADD: Add breadcrumb

--- web/src/pages/maintainance/UserMaintainancePage.tsx
+++ web/src/pages/maintainance/UserMaintainancePage.tsx
@@ -1,7 +1,23 @@
-import { useParams } from "@solidjs/router";
+import { usePageContext } from "@/contexts/PageContext";
+import GetUserQuery, { IGetUserRes } from "@/utils/queries/GetUserQuery";
+import { createAsync, useParams } from "@solidjs/router";
+import { createEffect, onMount } from "solid-js";
 
 export default function UserMaintainancePage() {
   const params = useParams();
+  const pageContext = usePageContext();
+  const getUserQuery = createAsync(() => GetUserQuery().query(params.id!));
+
+  onMount(() => {
+    pageContext.action.setEditMode(false);
+  });
+
+  createEffect(() => {
+    if (!getUserQuery()?.ok) return;
+
+    const user = (getUserQuery()?.data as IGetUserRes).result;
+    pageContext.action.setBreadcrumbs(["User Maintainance", user.name]);
+  });
 
   return <div>UserMaintainancePage : {params.id}</div>;
 }
