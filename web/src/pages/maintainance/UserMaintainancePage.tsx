8d246f1dd8d8df0a8ef90518c2c6d2f66be83b25
Anas Juwaidi <anas.didi95@tutamail.com>
2026-02-22 01:07:22 +0000

    ADD: Add edit user handler

--- web/src/pages/maintainance/UserMaintainancePage.tsx
+++ web/src/pages/maintainance/UserMaintainancePage.tsx
@@ -1,14 +1,29 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 import { usePageContext } from "@/contexts/PageContext";
-import UserForm from "@/forms/UserForm";
+import UserForm, { IUserModel } from "@/forms/UserForm";
+import UpdateUserAction from "@/utils/actions/UpdateUserAction";
 import GetUserQuery, { IGetUserRes } from "@/utils/queries/GetUserQuery";
-import { createAsync, useParams } from "@solidjs/router";
-import { createEffect, onMount } from "solid-js";
+import { createAsync, useAction, useParams } from "@solidjs/router";
+import { createEffect, onMount, Show } from "solid-js";
+import { useToast } from "solid-notifications";
 
 export default function UserMaintainancePage() {
   const params = useParams();
   const pageContext = usePageContext();
   const getUserQuery = createAsync(() => GetUserQuery().query(params.id!));
+  const updateUserAction = useAction(
+    UpdateUserAction([GetUserQuery().key]).action,
+  );
+  const { notify } = useToast();
+
+  const handleEdit = async (values: IUserModel) => {
+    const res = await updateUserAction(params.id!, { ...values });
+    if (res.ok) {
+      pageContext.action.setEditMode(false);
+      notify("Success", { type: "success" });
+    }
+  };
 
   onMount(() => {
     pageContext.action.setEditMode(false);
@@ -26,10 +41,25 @@ export default function UserMaintainancePage() {
       <UserForm
         isEditMode={pageContext.store.isEditMode}
         data={(getUserQuery()?.data as IGetUserRes)?.result}
-        onSubmit={() => {
-          /* empty */
-        }}
-      />
+        onSubmit={handleEdit}>
+        <Show
+          when={pageContext.store.isEditMode}
+          fallback={
+            <Button
+              label="Edit"
+              type="button"
+              color="primary"
+              onClick={() => pageContext.action.setEditMode(true)}
+            />
+          }>
+          <Button
+            label="Cancel"
+            type="button"
+            onClick={() => pageContext.action.setEditMode(false)}
+          />
+          <Button label="Update" type="submit" color="primary" />
+        </Show>
+      </UserForm>
     </Card>
   );
 }
